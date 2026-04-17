88abff3e40ad60f5302aa2879f1e744211434f88
EduardZlobin <leggv823@gmail.com>
2026-04-17 13:40:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -75,8 +75,8 @@ const APP = (document.body.dataset.app || "gladoon").toLowerCase();
 const IS_SKIBI = APP.includes("skibi");
 
 const WIFI_SSID = "WIFI-Pi33EMANIA";
-const WIFI_PASSWORD = "IDI_NAXYI";
-const PACKET_PASSWORD = "DO_ZABRODI";
+const WIFI_PASSWORD = process.env.IDI_NAXYI;
+const PACKET_PASSWORD = process.env.DO_ZABRODI;
 
 const SECRET_NOTES_CODE = "d18ed182d183d0b1d0b5d0b120d094d0b8d0bcd0b020d093d0bbd0b0d0b4d183d0bd";
 const INTERNAL_NOTES_URL = "gladoon://notes";
